eea104eaa90f1dd4140615f37986adadb03ecd53
Tom <45548991+tfpickard@users.noreply.github.com>
2025-11-18 17:25:01 -0700

    Build SvelteKit frontend as a static site

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,8 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: '200.html'
+    }),
     alias: {
       $lib: 'src/lib'
     }
